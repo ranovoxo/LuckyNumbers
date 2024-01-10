3078cc9abba09feb9278bff0147e29dd2de7b9ff
Randal Carr <randalrcarr@gmail.com>
2024-01-09 23:19:50 -0600

    fix key issue

--- lucky_numbers/settings.py
+++ lucky_numbers/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o8y(!h*fwj&as^w#qhw^8xyq=$%_$@k+yynbwfm1ciyovh5%92'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
